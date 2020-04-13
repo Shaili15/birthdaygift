ce97efea2694c238989241b4d69106c4401ef7cc
Shaili15 <60892796+Shaili15@users.noreply.github.com>
2020-04-13 18:05:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+var t=null;
+var displayImgCount = 0;
+
+
+window.onload = function(){
+    cycleImage();
+}
+
+function cycleImage(){
+    if (displayImgCount !== 0) {
+        document.getElementById(displayImgCount).style.display = "none";
+    }
+    displayImgCount = displayImgCount === 18 ? 1 : displayImgCount + 1;
+    document.getElementById(displayImgCount).style.display = "block";
+    if(t){
+        clearTimeout(t);
+        t = null;
+    }
+    t=setTimeout(cycleImage, 1000);
+}
+
+
+
+
+if (document.location.search.match(/type=embed/gi)) {
+    window.parent.postMessage('resize', "*");
+}
+
+window.open = function() {};
+window.print = function() {};
+// Support hover state for mobile.
+if (false) {
+    window.ontouchstart = function() {};
+}
